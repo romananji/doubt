e25ce99ae81bbd333295c31e345faefc396a6c87
romananji <romananji191@gmail.com>
2024-02-09 11:15:38 +0000

    adding app.js file

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const {addDays} = require('date-fns')
+
+app.get('/', function (request, response) {
+  let date = new Date()
+  let result = addDays(
+    new Date(date.getFullYear(), date.getMonth(), date.getDate()),
+    100,
+  )
+  response.send(
+    `${result.getDate()}/${result.getMonth() + 1}/${result.getFullYear()}`,
+  )
+})
+
+module.exports = app
+
+app.listen(3000)
